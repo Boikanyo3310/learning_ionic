234c679206adba58af01418bd3679342071f13a1
Boikanyo3310 <khaos3310@gmail.com>
2020-08-06 05:01:19 +0200

    Shopping list

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {ShoppingList} from './shopping-list';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+  shoppingList = new Array();
+  item;
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  addShoppingList()
+  {
+    this.shoppingList.push(new ShoppingList(this.item));
+  }
+  delete(i)
+  {
+    console.log(i);
+    this.shoppingList.splice(i, 1);
+  }
+}
---
+++ src/app/shopping-list.ts
@@ -0,0 +1,10 @@
+export class ShoppingList
+{
+    item: String;
+    checked: boolean;
+    constructor(item)
+    {
+        this.item = item;
+        this.checked = false;
+    }
+}
